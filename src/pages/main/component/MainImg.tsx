d9bd541050458ded08ed3093893b05cdf154c34d
crystal025 <oj3339@naver.com>
2022-12-24 23:35:16 +0900

    #6 [EDIT] main Img slider CSS

--- src/pages/main/component/MainImg.tsx
+++ src/pages/main/component/MainImg.tsx
@@ -12,47 +12,18 @@ const MainImg: React.FC = () => {
   );
 };
 const Container = styled.div`
-  display: inline-flex;
-  flex-basis: 35rem;
-  flex-direction: column;
-  flex-shrink: 1;
-  max-width: 35rem;
-  padding: 1em 3em;
   align-items: center;
-  text-align: center;
-  padding: 1rem 3%;
-  flex-basis: auto;
-  width: 100%;
-  max-width: 39rem;
-
-  /* @media screen {
-    max-aspect-ratio: 3 / 4;
-    min-height: 32rem;
-    max-width: 60rem;
-    min-height: 32rem;
-  } */
-  /* align-items: center;
-  display: -webkit-inline-box;
   display: inline-flex;
   flex-direction: column;
   flex-basis: 45%;
   overflow: hidden;
   padding: 1em 3em;
-  @media screen {
-    max-aspect-ratio: 3 / 4;
-    min-height: 32rem;
-    max-width: 60rem;
-    min-height: 32rem;
-    align-items: flex-start;
-    box-sizing: border-box;
-    overflow-y: auto;
-    padding: 1rem 3%;
-  } */
 `;
 
 const ImgBox = styled.div`
-  width: 52rem;
-  //max-width: 52rem;
-  height: 100%;
+  width: 100%;
+  max-width: 52rem;
+  height: 445px;
+  font-size: 1rem;
 `;
 export default MainImg;
