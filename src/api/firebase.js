8e1a3e1dee968961b71333f44237e1666f800707
0gunkim <ygunoffice@gmail.com>
2022-12-27 22:01:40 +0900

    [ADD] Firebase를 이용한 구글로그인

--- src/api/firebase.js
+++ src/api/firebase.js
@@ -2,6 +2,8 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getDatabase } from "firebase/database";
+import { GoogleAuthProvider } from "firebase/auth";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -20,5 +22,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+export const provider = new GoogleAuthProvider();
 export const db = getFirestore(app);
 export const database = getDatabase(app);
