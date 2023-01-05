7d392d4b171773a91dd1274bc63c5647bf9d236c
new-crystal <oj3339@naver.com>
2023-01-05 09:47:52 +0900

    [RM]git pull

---
+++ src/shared/Router.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Main from "../pages/main/Main";
+// import Join from "../pages/";
+import Chat from "../pages/meet/component/chat/Chat";
+
+const Router: React.FC = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Main />} />
+      <Route path="/meet/:roomId" element={<Chat />} />
+      {/* <Route path="/join/:roomId" element={<Join />} /> */}
+      <Route path="*" element={<Main />} />
+    </Routes>
+  );
+};
+
+export default Router;
