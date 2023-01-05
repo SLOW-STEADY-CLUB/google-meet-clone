1a73c9b32d8153dac3a1890b39706c98119bfa3e
new-crystal <oj3339@naver.com>
2023-01-05 15:31:23 +0900

    [EDIT] footer-right css 위치조정

--- src/pages/meet/component/footerLeft/FooterBox.jsx
+++ src/pages/meet/component/footerLeft/FooterBox.jsx
@@ -5,17 +5,20 @@ import { JoinCodeBox } from "./JoinCodeBox";
 const FooterBox = () => {
   return (
     <STContainer>
-      <JoinCodeBox></JoinCodeBox>
+      <JoinCodeBox />
       <FooterRight />
     </STContainer>
   );
 };
 const STContainer = styled.div`
+  width: 100vw;
   position: fixed;
   bottom: 0px;
   left: 0px;
   margin: 20px;
   display: flex;
   justify-content: space-between;
+  flex-direction: row;
+  align-items: center;
 `;
 export default FooterBox;
