96f713fa13317965cfe66ccf79b1bd97faca2948
Goldenprevue <goldenprevue@gmail.com>
2023-02-17 23:35:21 +0900

    [EDIT] 파이어베이스 api 변경 및 로그인 테스트

--- src/util/user/Login.jsx
+++ src/util/user/Login.jsx
@@ -1,94 +1,85 @@
 import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import { auth } from "../../server/firebase";
-import {
-  GoogleAuthProvider,
-  signInWithPopup,
-  signOut,
-} from "firebase/auth";
+import { GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
 import { setCookie, getCookie, removeCookie } from "../../shared/Cookie";
 
-
 const Login = () => {
-  
-const [userData, setUserData ] = useState();
+  // const [userData, setUserData ] = useState();
 
-const googleLoginHandler = () => {
-  const provider = new GoogleAuthProvider();
-  signInWithPopup(auth, provider).then((data)=> {
-    setUserData(data.user)
-    console.log(data)
-  })
-}
+  // const googleLoginHandler = () => {
+  //   const provider = new GoogleAuthProvider();
+  //   signInWithPopup(auth, provider).then((data)=> {
+  //     setUserData(data.user)
+  //     console.log(data)
+  //   })
+  // }
 
   // --------___-------
-  // const [authing, setAuthing] = useState(false);
-  // const [isLogin, setIsLogin] = useState(false);
-  // const isToken = getCookie("token");
-  // const imgUrl = getCookie("userImg");
+  const [authing, setAuthing] = useState(false);
+  const [isLogin, setIsLogin] = useState(false);
+  const isToken = getCookie("token");
+  const imgUrl = getCookie("userImg");
 
-  // const signInWithGoogle = async () => {
-  //   setAuthing(true);
+  const signInWithGoogle = async () => {
+    setAuthing(true);
 
-  //   signInWithPopup(auth, new GoogleAuthProvider())
-  //     .then(response => {
-  //       console.log(response);
-  //       console.log(response.user.uid);
-  //       console.log(response.user.email);
-  //       console.log(response.user.displayName);
-  //       const token = response.user.uid;
-  //       const userName = response.user.displayName;
-  //       const email = response.user.email;
-  //       const userImg = response.user.photoURL;
-  //       setCookie("token", token);
-  //       setCookie("userName", userName);
-  //       setCookie("email", email);
-  //       setCookie("userImg", userImg);
-  //     })
-  //     .catch(error => {
-  //       console.log(error);
-  //       setAuthing(false);
-  //     });
-  // };
+    signInWithPopup(auth, new GoogleAuthProvider())
+      .then(response => {
+        console.log(response);
+        console.log(response.user.uid);
+        console.log(response.user.email);
+        console.log(response.user.displayName);
+        const token = response.user.uid;
+        const userName = response.user.displayName;
+        const email = response.user.email;
+        const userImg = response.user.photoURL;
+        setCookie("token", token);
+        setCookie("userName", userName);
+        setCookie("email", email);
+        setCookie("userImg", userImg);
+      })
+      .catch(error => {
+        console.log(error);
+        setAuthing(false);
+      });
+  };
 
   const onClickLogin = () => {
-    googleLoginHandler();
-    // setIsLogin(true);
+    signInWithGoogle();
+    setIsLogin(true);
   };
   const onClickLogout = () => {
     signOut(auth);
     removeCookie("token");
     removeCookie("email");
     removeCookie("userName");
-    // setIsLogin(false);
+    setIsLogin(false);
   };
-  // useEffect(() => {
-  //   if (isToken !== undefined) {
-  //     setIsLogin(true);
-  //   } else {
-  //     setIsLogin(false);
-  //   }
-  // }, [isToken]);
+  useEffect(() => {
+    if (isToken !== undefined) {
+      setIsLogin(true);
+    } else {
+      setIsLogin(false);
+    }
+  }, [isToken]);
   return (
     <STLoginContainer>
-      <STButtonLogin onClick={onClickLogin}>로그인</STButtonLogin>
-      <STButton  onClick={onClickLogout}></STButton>
-      {/* {isLogin === false ? (
+      {isLogin === false ? (
+        <STButtonLogin onClick={onClickLogin}>로그인</STButtonLogin>
       ) : (
-      )} */}
-
-      {/* <LoginButton /> */}
+        <STButton onClick={onClickLogout}></STButton>
+      )}
     </STLoginContainer>
   );
 };
 const STLoginContainer = styled.div`
-margin-right: 20px;
-
-`
+  margin-right: 20px;
+`;
 const STButtonLogin = styled.div`
-font-size: 20px;
-font-style: bold;
-cursor: pointer;
+  font-size: 20px;
+  font-style: bold;
+  cursor: pointer;
 `;
 const STButton = styled.div`
   width: 40px;
